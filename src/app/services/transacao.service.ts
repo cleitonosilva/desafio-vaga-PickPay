da39c2c774663c0af0813f191815b74aeebd1c46
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-16 22:53:10 -0300

    Atualizando os metodos dos serviços

--- src/app/services/transacao.service.ts
+++ src/app/services/transacao.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { Transacao } from '../models/transacao'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransacaoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+private url = "https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989";
+
+// postTransacao(): Observable<Transacao[]> {
+//   return this.http.get<Transacao[]>(this.url); }
+
+  efetivarTransacao(transacao : Transacao[]) {
+    return  this.http.post(this.url, transacao);
+  }
 }
+
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { Usuario } from '../models/usuario'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  
+constructor(private http: HttpClient) { }
+private url = "https://www.mocky.io/v2/5d531c4f2e0000620081ddce";
 
-  constructor() { }
+getUsuarios(): Observable<Usuario[]> {
+  return this.http.get<Usuario[]>(this.url); 
+}
 }
