acd9613d440f65b168353a4f4a9bb7bb90df00be
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-16 22:53:51 -0300

    Chamando modal para pagamento

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { CardListComponent } from '../modal/card-list/card-list.component';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  usuarios: Usuario [] = []
+  nomeUsuarioparModal : string 
 
-  constructor() { }
+
+  constructor( private usuariosService : UsuariosService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
+
+    this.usuariosService.getUsuarios().subscribe(x =>
+      {this.usuarios = x})
+
   }
 
+  open() {
+    const modalRef = this.modalService.open(CardListComponent);
+    modalRef.componentInstance.name = this.nomeUsuarioparModal;
+  }
+
+  pegarNome(nome: any) {
+    this.nomeUsuarioparModal = nome.value
+
+  }
 }
