f7c372b188600191d996d033efd0b1951022c7b2
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-01-16 22:52:45 -0300

    importando dependencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardListComponent } from './components/modal/card-list/card-list.component';
+import { FormsModule }   from '@angular/forms';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    CurrencyMaskModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
